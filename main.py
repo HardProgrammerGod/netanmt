2b385e6bb0537d7e79e9e56647ef993d2353f511
Hard <herdbuilding1@gmail.com>
2026-08-26 01:35:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from bot.config import BOT_TOKEN
+from bot.admin import admin_router
+from bot.handlers import router
 
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(admin_router)
+    dp.include_router(router)
+    print("🚀 Бот netaNMT успешно запущен!")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
