07e6a77739154eaebe49193cfd196f462416b1c5
Hard <herdbuilding1@gmail.com>
2026-08-26 01:33:42 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,11 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+raw_admins = os.getenv("ADMIN_IDS", "")
+ADMIN_IDS = [int(i.strip()) for i in raw_admins.split(",") if i.strip().isdigit()]
